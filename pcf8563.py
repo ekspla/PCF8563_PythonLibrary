d865c5100b470dbc4d1aa3625b3d17dca9564f7c
ekspla <ekspla@sky.plala.or.jp>
2023-11-21 20:06:57 +0900

    MODs: read/write mutiple date & time registers at once

--- pcf8563.py
+++ pcf8563.py
@@ -1,4 +1,20 @@
 '''
+NOV. 2023, modified by ekspla.
+
+The code was modified to read/write mutiple date & time registers at once.
+
+CAUTION:
+  From the datasheets and application notes (e.g. AN10652), it is clear that 
+the date/time registers should be read/written in one single access (e.g. 
+before stop) and within 1 second for internal watchdog compensation to work; 
+the compensation is not useful for the lost two 1-Hz ticks.
+
+NOTE:
+  The definitions of weekday in machine.RTC depends on ports; e.g. a range 
+of [0,6] from MON to SUN in ESP32 while [1,7] in STM32 (Pyboard).  Adjust the 
+range appropriately.
+
+
 MIT License
 
 Copyright (c) 2019 lewis he
@@ -25,67 +41,95 @@ pcf8563.py - MicroPython library for NXP PCF8563 Real-time clock/calendar
 Created by Lewis he on September 17, 2019.
 github:https://github.com/lewisxhe/PCF8563_PythonLibrary
 '''
-import utime
-from machine import I2C
+import time
+from micropython import const
 
-PCF8563_SLAVE_ADDRESS = const(0x51)
-PCF8563_STAT1_REG = const(0x00)
-PCF8563_STAT2_REG = const(0x01)
-PCF8563_SEC_REG = const(0x02)
-PCF8563_MIN_REG = const(0x03)
-PCF8563_HR_REG = const(0x04)
-PCF8563_DAY_REG = const(0x05)
-PCF8563_WEEKDAY_REG = const(0x06)
-PCF8563_MONTH_REG = const(0x07)
-PCF8563_YEAR_REG = const(0x08)
-PCF8563_SQW_REG = const(0x0D)
-PCF8563_TIMER1_REG = const(0x0E)
-PCF8563_TIMER2_REG = const(0x0F)
-PCF8563_VOL_LOW_MASK = const(0x80)
-PCF8563_minuteS_MASK = const(0x7F)
-PCF8563_HOUR_MASK = const(0x3F)
-PCF8563_WEEKDAY_MASK = const(0x07)
-PCF8563_CENTURY_MASK = const(0x80)
-PCF8563_DAY_MASK = const(0x3F)
-PCF8563_MONTH_MASK = const(0x1F)
-PCF8563_TIMER_CTL_MASK = const(0x03)
-PCF8563_ALARM_AF = const(0x08)
-PCF8563_TIMER_TF = const(0x04)
-PCF8563_ALARM_AIE = const(0x02)
-PCF8563_TIMER_TIE = const(0x01)
-PCF8563_TIMER_TE = const(0x80)
-PCF8563_TIMER_TD10 = const(0x03)
-PCF8563_NO_ALARM = const(0xFF)
-PCF8563_ALARM_ENABLE = const(0x80)
-PCF8563_CLK_ENABLE = const(0x80)
-PCF8563_ALARM_MINUTES = const(0x09)
-PCF8563_ALARM_HOURS = const(0x0A)
-PCF8563_ALARM_DAY = const(0x0B)
-PCF8563_ALARM_WEEKDAY = const(0x0C)
+_SLAVE_ADDRESS = const(0x51)
 
-CLOCK_CLK_OUT_FREQ_32_DOT_768KHZ = const(0x80)
-CLOCK_CLK_OUT_FREQ_1_DOT_024KHZ = const(0x81)
-CLOCK_CLK_OUT_FREQ_32_KHZ = const(0x82)
-CLOCK_CLK_OUT_FREQ_1_HZ = const(0x83)
-CLOCK_CLK_HIGH_IMPEDANCE = const(0x0)
+_STAT1_REG = const(0x00)
+_STAT2_REG = const(0x01)
+
+_SEC_REG = _DATETIME_REG = const(0x02)
+_MIN_REG = const(0x03)
+_HR_REG = const(0x04)
+_DATE_REG = const(0x05)
+_WEEKDAY_REG = const(0x06)
+_MONTH_REG = const(0x07)
+_YEAR_REG = const(0x08)
+
+_ALARM_MINUTES_REG = _ALARMS_REG = const(0x09)
+_ALARM_HOURS_REG = const(0x0A)
+_ALARM_DATE_REG = const(0x0B)
+_ALARM_WEEKDAY_REG = const(0x0C)
+
+_SQW_REG = const(0x0D)
+
+_TIMER1_REG = _TIMERS_REG = const(0x0E)
+_TIMER2_REG = const(0x0F)
+
+_VOL_LOW_MASK = const(0x80)
+_minuteS_MASK = const(0x7F)
+_HOUR_MASK = const(0x3F)
+_WEEKDAY_MASK = const(0x07)
+_YEAR_MASK = const(0xff)
+_CENTURY_MASK = const(0x80)
+_DATE_MASK = const(0x3F)
+_MONTH_MASK = const(0x1F)
+_TIMER_CTL_MASK = const(0x03)
+
+_STOP = const(0x20)
+_ALARM_AF = const(0x08)
+_TIMER_TF = const(0x04)
+_ALARM_AIE = const(0x02)
+_TIMER_TIE = const(0x01)
+_TIMER_TE = const(0x80)
+_TIMER_TD10 = const(0x03)
+_NO_ALARM = const(0xFF)
+_ALARM_ENABLE = const(0x80)
+_CLK_ENABLE = const(0x80)
+
+CLK_OUT_FREQ_32_DOT_768KHZ = const(0x80)
+CLK_OUT_FREQ_1_DOT_024KHZ = const(0x81)
+CLK_OUT_FREQ_32_HZ = const(0x82)
+CLK_OUT_FREQ_1_HZ = const(0x83)
+CLK_HIGH_IMPEDANCE = const(0x0)
 
 
 class PCF8563:
-    def __init__(self, i2c, address=None):
+    def __init__(self, i2c, address=_SLAVE_ADDRESS, en_subsecond=False):
         """Initialization needs to be given an initialized I2C port
         """
         self.i2c = i2c
-        self.address = address if address else PCF8563_SLAVE_ADDRESS
-        self.buffer = bytearray(16)
-        self.bytebuf = memoryview(self.buffer[0:1])
+        self.address = address
+        self._buffer = bytearray(16)
+        self._bytebuf = bytearray(1)
+        self._mv = memoryview(self._buffer)
+        self._mv_datetime = self._mv[_SEC_REG:_YEAR_REG + 1]
+        self._mv_timers = self._mv[_TIMER1_REG:_TIMER2_REG + 1]
+        self._mv_alarms = self._mv[_ALARM_MINUTES_REG:_ALARM_WEEKDAY_REG + 1]
+        self._DATETIME_MASK = bytes((
+            _minuteS_MASK, 
+            _minuteS_MASK, 
+            _HOUR_MASK, 
+            _DATE_MASK, 
+            _WEEKDAY_MASK, 
+            _MONTH_MASK, 
+            _YEAR_MASK))
+        self._en_subsecond = en_subsecond
 
     def __write_byte(self, reg, val):
-        self.bytebuf[0] = val
-        self.i2c.writeto_mem(self.address, reg, self.bytebuf)
+        self._bytebuf[0] = val & 0xff
+        self.__write_bytes(reg, self._bytebuf)
 
     def __read_byte(self, reg):
-        self.i2c.readfrom_mem_into(self.address, reg, self.bytebuf)
-        return self.bytebuf[0]
+        self.__read_bytes(reg, self._bytebuf)
+        return self._bytebuf[0]
+
+    def __write_bytes(self, reg, buffer):
+        self.i2c.writeto_mem(self.address, reg, buffer)
+
+    def __read_bytes(self, reg, buffer):
+        self.i2c.readfrom_mem_into(self.address, reg, buffer)
 
     def __bcd2dec(self, bcd):
         return (((bcd & 0xf0) >> 4) * 10 + (bcd & 0x0f))
@@ -94,95 +138,78 @@ class PCF8563:
         tens, units = divmod(dec, 10)
         return (tens << 4) + units
 
-    def seconds(self):
-        """Get the current allowed seconds of PCF8563
-        """
-        return self.__bcd2dec(self.__read_byte(PCF8563_SEC_REG) & 0x7F)
-
-    def minutes(self):
-        """Get the current allowed minutes of PCF8563
-        """
-        return self.__bcd2dec(self.__read_byte(PCF8563_MIN_REG) & 0x7F)
-
-    def hours(self):
-        """Get the current allowed hours of PCF8563
-        """
-        d = self.__read_byte(PCF8563_HR_REG) & 0x3F
-        return self.__bcd2dec(d & 0x3F)
-
-    def day(self):
-        """Get the current allowed day of PCF8563
-        """
-        return self.__bcd2dec(self.__read_byte(PCF8563_WEEKDAY_REG) & 0x07)
-
-    def date(self):
-        """Get the current allowed date of PCF8563
-        """
-        return self.__bcd2dec(self.__read_byte(PCF8563_DAY_REG) & 0x3F)
-
-    def month(self):
-        """Get the current allowed month of PCF8563
-        """
-        return self.__bcd2dec(self.__read_byte(PCF8563_MONTH_REG) & 0x1F)
-
-    def year(self):
-        """Get the current allowed year of PCF8563
-        """
-        return self.__bcd2dec(self.__read_byte(PCF8563_YEAR_REG))
-
     def datetime(self):
-        """Return a tuple such as (year, month, date, day, hours, minutes,
-        seconds).
+        """Return a tuple such as (year, month, date, weekday, hours, minutes,
+        seconds, subseconds[63,0]).
         """
-        return (self.year(), self.month(), self.date(),
-                self.day(), self.hours(), self.minutes(),
-                self.seconds())
+        if self._en_subsecond:
+            self.__read_bytes(_TIMERS_REG, self._mv_timers)
+        self.__read_bytes(_DATETIME_REG, self._mv_datetime)
 
-    def write_all(self, seconds=None, minutes=None, hours=None, day=None,
+        seconds, minutes, hours, date, weekday, month, year = (
+            self.__bcd2dec(a & b) for a, b in zip(
+            self._mv_datetime, self._DATETIME_MASK))
+
+        if self._en_subsecond:
+            return (year, month, date, weekday, hours, minutes, seconds, 
+                self._buffer[_TIMER2_REG] & 0x7f)
+        return (year, month, date, weekday, hours, minutes, seconds)
+
+    def write_all(self, seconds=None, minutes=None, hours=None, weekday=None,
                   date=None, month=None, year=None):
         """Direct write un-none value.
         Range: seconds [0,59], minutes [0,59], hours [0,23],
-               day [0,7], date [1-31], month [1-12], year [0-99].
+               weekday [0,6], date [1,31], month [1,12], year [0,99].
         """
-        if seconds is not None:
-            if seconds < 0 or seconds > 59:
-                raise ValueError('Seconds is out of range [0,59].')
-            seconds_reg = self.__dec2bcd(seconds)
-            self.__write_byte(PCF8563_SEC_REG, seconds_reg)
+        if (seconds is None) or seconds < 0 or seconds > 59:
+            raise ValueError('Seconds is out of range [0,59].')
+        if (minutes is None) or minutes < 0 or minutes > 59:
+            raise ValueError('Minutes is out of range [0,59].')
+        if (hours is None) or hours < 0 or hours > 23:
+            raise ValueError('Hours is out of range [0,23].')
+        if (date is None) or date < 1 or date > 31:
+            raise ValueError('Date is out of range [1,31].')
+        if (weekday is None) or weekday < 0 or weekday > 6:
+            raise ValueError('Day is out of range [0,6].')
+        if (month is None) or month < 1 or month > 12:
+            raise ValueError('Month is out of range [1,12].')
+        if (year is None) or year < 0 or year > 99:
+            raise ValueError('Years is out of range [0,99].')
 
-        if minutes is not None:
-            if minutes < 0 or minutes > 59:
-                raise ValueError('Minutes is out of range [0,59].')
-            self.__write_byte(PCF8563_MIN_REG, self.__dec2bcd(minutes))
+        self._buffer[_SEC_REG] = self.__dec2bcd(seconds)
+        self._buffer[_MIN_REG] = self.__dec2bcd(minutes)
+        self._buffer[_HR_REG] = self.__dec2bcd(hours) # no 12 hour mode
+        self._buffer[_DATE_REG] = self.__dec2bcd(date)
+        self._buffer[_WEEKDAY_REG] = self.__dec2bcd(weekday)
+        self._buffer[_MONTH_REG] = self.__dec2bcd(month)
+        self._buffer[_YEAR_REG] = self.__dec2bcd(year)
 
-        if hours is not None:
-            if hours < 0 or hours > 23:
-                raise ValueError('Hours is out of range [0,23].')
-            # no 12 hour mode
-            self.__write_byte(PCF8563_HR_REG, self.__dec2bcd(hours))
+        if self._en_subsecond:
+            self.stop()
+            self.__write_bytes(_DATETIME_REG, self._mv_datetime)
+            self._buffer[_STAT2_REG] = self.__read_byte(_STAT2_REG) & ~_TIMER_TIE
+            self.__write_byte(_STAT2_REG, self._buffer[_STAT2_REG]) # TIE disable
+            self._buffer[_TIMER1_REG] = _TIMER_TE | b'\01' # TE enable, source clk 64 Hz
+            self._buffer[_TIMER2_REG] = b'\x40' # Set Countdown Timer to 64
+            self.__write_bytes(_TIMERS_REG, self._mv_timers)
+            self.start()
 
-        if year is not None:
-            if year < 0 or year > 99:
-                raise ValueError('Years is out of range [0,99].')
-            self.__write_byte(PCF8563_YEAR_REG, self.__dec2bcd(year))
+        else:
+            # STOP/START not necessary because of the compensation by internal watchdog.
+            self.__write_bytes(_DATETIME_REG, self._mv_datetime)
 
-        if month is not None:
-            if month < 1 or month > 12:
-                raise ValueError('Month is out of range [1,12].')
-            self.__write_byte(PCF8563_MONTH_REG, self.__dec2bcd(month))
+    def stop(self):
+        """Stop divided clocks below 4096 Hz in frequency"""
+        self._buffer[_STAT1_REG] = self.__read_byte(_STAT1_REG) | _STOP
+        self.__write_byte(_STAT1_REG, self._buffer[_STAT1_REG])
 
-        if date is not None:
-            if date < 1 or date > 31:
-                raise ValueError('Date is out of range [1,31].')
-            self.__write_byte(PCF8563_DAY_REG, self.__dec2bcd(date))
-
-        if day is not None:
-            if day < 1 or day > 7:
-                raise ValueError('Day is out of range [1,7].')
-            self.__write_byte(PCF8563_WEEKDAY_REG, self.__dec2bcd(day))
+    def start(self):
+        """Start divided clocks below 4096 Hz in frequency"""
+        self._buffer[_STAT1_REG] = self.__read_byte(_STAT1_REG) & ~_STOP
+        self.__write_byte(_STAT1_REG, self._buffer[_STAT1_REG])
 
     def set_datetime(self, dt):
-        """Input a tuple such as (year, month, date, day, hours, minutes,
+        """Input a tuple such as (year, month, date, weekday, hours, minutes,
         seconds).
         """
         self.write_all(dt[5], dt[4], dt[3],
@@ -191,93 +218,89 @@ class PCF8563:
     def write_now(self):
         """Write the current system time to PCF8563
         """
-        self.set_datetime(utime.localtime())
+        self.set_datetime(time.localtime())
 
-    def set_clk_out_frequency(self, frequency=CLOCK_CLK_OUT_FREQ_1_HZ):
+    def set_clk_out_frequency(self, frequency=CLK_OUT_FREQ_1_HZ):
         """Set the clock output pin frequency
         """
-        self.__write_byte(PCF8563_SQW_REG, frequency)
+        self._buffer[_SQW_REG] = frequency
+        self.__write_byte(_SQW_REG, self._buffer[_SQW_REG])
 
     def check_if_alarm_on(self):
         """Read the register to get the alarm enabled
         """
-        return bool(self.__read_byte(PCF8563_STAT2_REG) & PCF8563_ALARM_AF)
+        return bool(self.__read_byte(_STAT2_REG) & _ALARM_AF)
 
     def turn_alarm_off(self):
         """Should not affect the alarm interrupt state.
         """
-        alarm_state = self.__read_byte(PCF8563_STAT2_REG)
-        self.__write_byte(PCF8563_STAT2_REG, alarm_state & 0xf7)
+        self._buffer[_STAT2_REG] = self.__read_byte(_STAT2_REG) & ~_ALARM_AF
+        self.__write_byte(_STAT2_REG, self._buffer[_STAT2_REG])
 
     def clear_alarm(self):
         """Clear status register.
         """
-        alarm_state = self.__read_byte(PCF8563_STAT2_REG)
-        alarm_state &= ~(PCF8563_ALARM_AF)
-        alarm_state |= PCF8563_TIMER_TF
-        self.__write_byte(PCF8563_STAT2_REG, alarm_state)
+        self._buffer[_STAT2_REG] = self.__read_byte(_STAT2_REG)
+        self._buffer[_STAT2_REG] &= ~_ALARM_AF
+        self._buffer[_STAT2_REG] |= _TIMER_TF
 
-        self.__write_byte(PCF8563_ALARM_MINUTES, 0x80)
-        self.__write_byte(PCF8563_ALARM_HOURS, 0x80)
-        self.__write_byte(PCF8563_ALARM_DAY, 0x80)
-        self.__write_byte(PCF8563_ALARM_WEEKDAY, 0x80)
+        self._buffer[_ALARM_MINUTES_REG] = self._buffer[_ALARM_HOURS_REG] \
+            = self._buffer[_ALARM_DATE_REG] = self._buffer[_ALARM_WEEKDAY_REG] \
+            = _ALARM_ENABLE
+
+        self.__write_byte(_STAT2_REG, self._buffer[_STAT2_REG])
+        self.__write_bytes(_ALARMS_REG, self._mv_alarms)
 
     def check_for_alarm_interrupt(self):
         """check for alarm interrupt,is alram int return True
         """
-        return bool(self.__read_byte(PCF8563_STAT2_REG) & 0x02)
+        return bool(self.__read_byte(_STAT2_REG) & _ALARM_AIE)
 
     def enable_alarm_interrupt(self):
         """Turn on the alarm interrupt output to the interrupt pin
         """
-        alarm_state = self.__read_byte(PCF8563_STAT2_REG)
-        alarm_state &= ~PCF8563_ALARM_AF
-        alarm_state |= (PCF8563_TIMER_TF | PCF8563_ALARM_AIE)
-        self.__write_byte(PCF8563_STAT2_REG, alarm_state)
+        self._buffer[_STAT2_REG] = self.__read_byte(_STAT2_REG)
+        self._buffer[_STAT2_REG] &= ~_ALARM_AF
+        self._buffer[_STAT2_REG] |= (_TIMER_TF | _ALARM_AIE)
+        self.__write_byte(_STAT2_REG, self._buffer[_STAT2_REG])
 
     def disable_alarm_interrupt(self):
         """Turn off the alarm interrupt output to the interrupt pin
         """
-        alarm_state = self.__read_byte(PCF8563_STAT2_REG)
-        alarm_state &= ~(PCF8563_ALARM_AF | PCF8563_ALARM_AIE)
-        alarm_state |= PCF8563_TIMER_TF
-        self.__write_byte(PCF8563_STAT2_REG, alarm_state)
+        self._buffer[_STAT2_REG] = self.__read_byte(_STAT2_REG)
+        self._buffer[_STAT2_REG] &= ~(_ALARM_AF | _ALARM_AIE)
+        self._buffer[_STAT2_REG] |= _TIMER_TF
+        self.__write_byte(_STAT2_REG, self._buffer[_STAT2_REG])
 
     def set_daily_alarm(self, hours=None, minutes=None, date=None, weekday=None):
         """Set alarm match, allow sometimes, minute, day, week
         """
         if minutes is None:
-            minutes = PCF8563_ALARM_ENABLE
-            self.__write_byte(PCF8563_ALARM_MINUTES, minutes)
+            self._buffer[_ALARM_MINUTES_REG] = _ALARM_ENABLE
         else:
             if minutes < 0 or minutes > 59:
                 raise ValueError('Minutes is out of range [0,59].')
-            self.__write_byte(PCF8563_ALARM_MINUTES,
-                            self.__dec2bcd(minutes) & 0x7f)
+            self._buffer[_ALARM_MINUTES_REG] = self.__dec2bcd(minutes) & _minuteS_MASK
 
         if hours is None:
-            hours = PCF8563_ALARM_ENABLE
-            self.__write_byte(PCF8563_ALARM_HOURS, hours)
+            self._buffer[_ALARM_HOURS_REG] = _ALARM_ENABLE
         else:
             if hours < 0 or hours > 23:
                 raise ValueError('Hours is out of range [0,23].')
-            self.__write_byte(PCF8563_ALARM_HOURS, self.__dec2bcd(
-                hours) & 0x7f)
+            self._buffer[_ALARM_HOURS_REG] = self.__dec2bcd(hours) & _HOUR_MASK
 
         if date is None:
-            date = PCF8563_ALARM_ENABLE
-            self.__write_byte(PCF8563_ALARM_DAY, date)
+            self._buffer[_ALARM_DATE_REG] = _ALARM_ENABLE
         else:
             if date < 1 or date > 31:
                 raise ValueError('date is out of range [1,31].')
-            self.__write_byte(PCF8563_ALARM_DAY, self.__dec2bcd(
-                date) & 0x7f)
+            self._buffer[_ALARM_DATE_REG] = self.__dec2bcd(date) & _DATE_MASK
 
         if weekday is None:
-            weekday = PCF8563_ALARM_ENABLE
-            self.__write_byte(PCF8563_ALARM_WEEKDAY, weekday)
+            self._buffer[_ALARM_WEEKDAY_REG] = _ALARM_ENABLE
         else:
             if weekday < 0 or weekday > 6:
                 raise ValueError('weekday is out of range [0,6].')
-            self.__write_byte(PCF8563_ALARM_WEEKDAY, self.__dec2bcd(
-                weekday) & 0x7f)
+            self._buffer[_ALARM_WEEKDAY_REG] = self.__dec2bcd(weekday) & _WEEKDAY_MASK
+
+        self.__write_bytes(_ALARMS_REG, self._mv_alarms)
